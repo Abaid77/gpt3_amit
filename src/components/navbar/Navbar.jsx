14f0739effee36b354e3ec1e93d1a1ab97c0c788
Amit Baid <baidstar@hotmail.com>
2023-01-12 11:56:04 -0700

    build header

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = () => {
       </div>
       <div className="gpt3__navbar-sign">
         <p>Sign in</p>
-        <button type="button">Sign in</button>
+        <button type="button">Sign up</button>
       </div>
       <div className="gpt3__navbar-menu">
         {toggleMenu ? (
@@ -57,9 +57,9 @@ const Navbar = () => {
           <div className="gpt3__navbar-menu_container scale-up-center">
             <div className="gpt3__navbar-menu_container-links">
               <Menu />
-              <div className="gpt3__navbar-menu_container-sign">
+              <div className="gpt3__navbar-menu_container-links-sign">
                 <p>Sign in</p>
-                <button type="button">Sign in</button>
+                <button type="button">Sign up</button>
               </div>
             </div>
           </div>
